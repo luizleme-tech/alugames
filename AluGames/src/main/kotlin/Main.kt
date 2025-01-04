61b91092d00d1d8e429f05fdfc9911823ec0fe7d
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-01-04 18:36:44 -0300

    refactor(feature/tratamento-de-erros-refatoracao): refatorada a implementação para ler um código de jogo; Montado o endereço com a url e com o código lido; Realizado o tratamento de exceção com o runCatching, implementado o comportamento em caso de falha e o em caso de sucesso.

--- AluGames/src/main/kotlin/Main.kt
+++ AluGames/src/main/kotlin/Main.kt
@@ -7,25 +7,58 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse.BodyHandlers
+import java.util.Scanner
 
 
 fun main() {
+
+    val leitura = Scanner(System.`in`)
+    println("Digite um código de jogo para buscar:")
+    val busca = leitura.nextLine()
+
+    val endereco = "https://www.cheapshark.com/api/1.0/games?id=$busca"
+
     val client: HttpClient = HttpClient.newHttpClient()
     val request = HttpRequest.newBuilder()
-        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .uri(URI.create(endereco))
         .build()
+
     val response = client
         .send(request, BodyHandlers.ofString())
 
     val json = response.body()
-    println(json)
+    //println(json)
 
     val gson = Gson()
-    val meuInfoJogo = gson.fromJson(json, InfoJogo::class.java)
 
-    val meuJogo = Jogo(
-        meuInfoJogo.info.title,
-        meuInfoJogo.info.thumb)
+    var meuJogo:Jogo? = null
 
-    println(meuJogo)
+    val resultado = runCatching {
+        val meuInfoJogo = gson.fromJson(json, InfoJogo::class.java)
+            meuJogo = Jogo(
+            meuInfoJogo.info.title,
+            meuInfoJogo.info.thumb
+        )
+    }
+
+    resultado.onFailure {
+        println("Jogo inexistente. Tente outro id.")
+    }
+
+    resultado.onSuccess {
+        println("Deseja inserir uma descrição personalizada? S/N")
+        val opcao = leitura.nextLine()
+        if (opcao.equals("s", true)) {
+            println("Insira a descrição personalizada para o jogo:")
+            var descricaoPersonalizada = leitura.nextLine()
+            meuJogo?.descricao = descricaoPersonalizada
+        } else {
+            meuJogo?.descricao = meuJogo?.titulo
+        }
+        println(meuJogo)
+    }
+
+    resultado.onSuccess {
+        println("Busca finalizada com sucesso")
+    }
 }
