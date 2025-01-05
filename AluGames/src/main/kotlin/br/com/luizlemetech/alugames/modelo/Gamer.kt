61f444e654a28af60b4fd7bef3f83c8a1d885871
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-01-04 23:05:27 -0300

    feat(feature/aprimorando-busca): Implementado método de validação de email com uso de regex; Uso do método init para validar o nome e email

--- AluGames/src/main/kotlin/br/com/luizlemetech/alugames/modelo/Gamer.kt
+++ AluGames/src/main/kotlin/br/com/luizlemetech/alugames/modelo/Gamer.kt
@@ -21,13 +21,29 @@ data class Gamer(var nome: String, var email: String) {
         criarIdInterno()
     }
 
+    init {
+        if(nome.isNullOrBlank()) {
+            throw IllegalArgumentException("Nome está em branco")
+        }
+        this.email = validarEmail()
+    }
+
     override fun toString(): String {
         return "Gamer(nome='$nome', email='$email', dataNascimento=$dataNascimento, usuario=$usuario, idInterno=$idInterno)"
     }
 
-    fun criarIdInterno() {
+    private fun criarIdInterno() {
         val numero = Random.nextInt(10000)
         val tag = String.format("%04d", numero)
         idInterno = "$usuario#$tag"
+    }
+
+    fun validarEmail():String {
+        val regex = Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
+        if(regex.matches(email)){
+            return email
+        } else {
+            throw IllegalArgumentException("Email inválido")
+        }
     }
 }
