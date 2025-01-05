8105ebb4b5c3b9b8a2381b58ffd4415211f41300
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-01-05 17:19:26 -0300

    feat(feature/6.aplicando-boas-praticas-em-kotlin): implementado novo construtor que recebe o valor de preco

--- AluGames/src/main/kotlin/br/com/luizlemetech/alugames/modelo/Jogo.kt
+++ AluGames/src/main/kotlin/br/com/luizlemetech/alugames/modelo/Jogo.kt
@@ -3,8 +3,14 @@ package br.com.luizlemetech.alugames.modelo
 data class Jogo(val titulo:String,
            val capa:String) {
     var descricao:String? = null
+    var preco:Double? = 0.0
 
     constructor(titulo: String, capa: String, descricao:String):this(titulo, capa){
+        this.descricao = descricao
+    }
+
+    constructor(titulo: String, capa: String, preco:Double, descricao: String):this(titulo, capa){
+        this.preco = preco
         this.descricao = descricao
     }
 
@@ -12,6 +18,7 @@ data class Jogo(val titulo:String,
         return "\n[Jogo]\n" +
                 "Título: $titulo \n" +
                 "Capa: $capa \n" +
-                "Descricao: $descricao"
+                "Descricao: $descricao \n" +
+                "Preco: $preco \n"
     }
 }
