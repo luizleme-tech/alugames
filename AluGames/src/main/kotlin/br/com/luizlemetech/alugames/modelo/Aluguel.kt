be8b182492d160bd4871ec47afa53fa0b8605c1c
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-01-06 21:30:53 -0300

    refactor(feature/7.iniciando-aluguel-jogos): Alterada a implementacao para receber um periodo e calcular o valor do aluguel, ajuste do toString

--- AluGames/src/main/kotlin/br/com/luizlemetech/alugames/modelo/Aluguel.kt
+++ AluGames/src/main/kotlin/br/com/luizlemetech/alugames/modelo/Aluguel.kt
@@ -1,10 +1,16 @@
 package br.com.luizlemetech.alugames.modelo
 
+import java.time.LocalDate
+import java.time.Period
+
 data class Aluguel(
-    val gamer:Gamer,
-    val jogo:Jogo) {
+    val gamer: Gamer,
+    val jogo: Jogo,
+    val periodo: Periodo,
+    val valorDoAluguel: Double = (jogo.preco * periodo.emDias)
+) {
 
     override fun toString(): String {
-        return "Aluguel do ${jogo.titulo} por ${gamer.nome}"
+        return "Aluguel do ${jogo.titulo} por ${gamer.nome} pelo valor R$$valorDoAluguel"
     }
 }
