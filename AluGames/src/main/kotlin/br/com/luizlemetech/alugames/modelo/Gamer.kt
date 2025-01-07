627aebffec0f46730ce389ee7bae10cd5c4e347b
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-01-06 22:14:14 -0300

    feat(feature/8.incluind-planos-pagamento): alterado o tipo do plano

--- AluGames/src/main/kotlin/br/com/luizlemetech/alugames/modelo/Gamer.kt
+++ AluGames/src/main/kotlin/br/com/luizlemetech/alugames/modelo/Gamer.kt
@@ -16,7 +16,7 @@ data class Gamer(var nome: String, var email: String) {
         private set
     val jogosBuscados = mutableListOf<Jogo?>()
     val jogosAlugados = mutableListOf<Aluguel>()
-    var plano = PlanoAvulso("BRONZE")
+    var plano: Plano = PlanoAvulso("BRONZE")
 
     constructor(nome: String, email: String, dataNascimento: String, usuario: String) :
             this(nome, email) {
