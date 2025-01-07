d9741a71bf81491e554d8b2c71491717b3c3ba92
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-01-06 21:32:19 -0300

    feat(feature/7.iniciando-aluguel-jogos): implementado os testes

--- AluGames/src/main/kotlin/br/com/luizlemetech/alugames/principal/TestJson.kt
+++ AluGames/src/main/kotlin/br/com/luizlemetech/alugames/principal/TestJson.kt
@@ -1,6 +1,8 @@
 package br.com.luizlemetech.alugames.principal
 
+import br.com.luizlemetech.alugames.modelo.Periodo
 import br.com.luizlemetech.alugames.servicos.ConsumoApi
+import java.time.LocalDate
 
 fun main() {
     val consumo = ConsumoApi()
@@ -9,11 +11,16 @@ fun main() {
 
     val gamerCaroline = listaGamers.get(3)
     val jogoResidenteVillage = listaJogos.get(10)
+    val jogoSpider = listaJogos.get(13)
+    val jogoTheLastOfUs = listaJogos.get(2)
 
-    println(gamerCaroline)
-    println(jogoResidenteVillage)
+    val periodo1 = Periodo(LocalDate.now(), LocalDate.now().plusDays(7))
+    val periodo2 = Periodo(LocalDate.now(), LocalDate.now().plusDays(3))
+    val periodo3 = Periodo(LocalDate.now(), LocalDate.now().plusDays(10))
 
-    val aluguel = gamerCaroline.alugaJogo(jogoResidenteVillage)
+    gamerCaroline.alugaJogo(jogoResidenteVillage, periodo1)
+    gamerCaroline.alugaJogo(jogoSpider, periodo2)
+    gamerCaroline.alugaJogo(jogoTheLastOfUs, periodo3)
 
-    println(aluguel)
+    println(gamerCaroline.jogosAlugados)
 }
