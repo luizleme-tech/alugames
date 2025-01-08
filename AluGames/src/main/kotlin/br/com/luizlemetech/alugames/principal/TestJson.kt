cd4e3d7c258edbe49daba4bd7110edd1e6976961
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-01-07 22:30:44 -0300

    refactor(feature/10.adicionando-recomendacoes-projeto): ajuste nos testes

--- AluGames/src/main/kotlin/br/com/luizlemetech/alugames/principal/TestJson.kt
+++ AluGames/src/main/kotlin/br/com/luizlemetech/alugames/principal/TestJson.kt
@@ -3,6 +3,9 @@ package br.com.luizlemetech.alugames.principal
 import br.com.luizlemetech.alugames.modelo.Periodo
 import br.com.luizlemetech.alugames.modelo.PlanoAssinatura
 import br.com.luizlemetech.alugames.servicos.ConsumoApi
+import com.google.gson.GsonBuilder
+import java.io.File
+import java.math.BigDecimal
 import java.time.LocalDate
 
 fun main() {
@@ -25,20 +28,41 @@ fun main() {
     //println(gamerCaroline.jogosAlugados)
 
     val gamerCamila = listaGamers.get(5)
-    gamerCamila.plano = PlanoAssinatura("PRATA", 9.90, 3, 0.15)
+    gamerCamila.plano = PlanoAssinatura("PRATA", 9.90, 3, BigDecimal("0.15"))
 
     gamerCamila.alugaJogo(jogoResidenteVillage, periodo1)
     gamerCamila.alugaJogo(jogoSpider, periodo2)
     gamerCamila.alugaJogo(jogoTheLastOfUs, periodo3)
     gamerCamila.alugaJogo(jogoTheLastOfUs, periodo3)
-    println(gamerCamila.jogosAlugados)
+//    println(gamerCamila.jogosAlugados)
 
     gamerCamila.recomendar(7)
     gamerCamila.recomendar(10)
     gamerCamila.recomendar(8)
-    println(gamerCamila)
+//    println(gamerCamila)
 
     gamerCamila.alugaJogo(jogoResidenteVillage, periodo1)
-    println(gamerCamila.jogosAlugados)
+//    println(gamerCamila.jogosAlugados)
 
+    gamerCamila.recomendarJogo(jogoResidenteVillage, 7)
+    gamerCamila.recomendarJogo(jogoTheLastOfUs, 10)
+
+    gamerCaroline.recomendarJogo(jogoResidenteVillage, 8)
+    gamerCaroline.recomendarJogo(jogoTheLastOfUs, 9)
+
+    println("Recomendacoes da Caroline")
+    println(gamerCaroline.jogosRecomendados)
+    println("Recomendacoes da Camila")
+    println(gamerCamila.jogosRecomendados)
+
+    val gson = GsonBuilder()
+        .excludeFieldsWithoutExposeAnnotation()
+        .create()
+    val serializacao = gson.toJson(gamerCamila.jogosRecomendados)
+    println(serializacao)
+
+    val arquivo = File("jogosRecomendados-${gamerCamila.nome}.json")
+    arquivo.writeText(serializacao)
+
+    println(arquivo.absolutePath)
 }
