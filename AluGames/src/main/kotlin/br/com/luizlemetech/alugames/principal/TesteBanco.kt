44ac50ef537410efbe70f5bf9cdae72a8501d3c3
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-01-10 22:45:18 -0300

    feat(feature/11.persistindo-dados-com-hibernate): ajustes para testes

--- AluGames/src/main/kotlin/br/com/luizlemetech/alugames/principal/TesteBanco.kt
+++ AluGames/src/main/kotlin/br/com/luizlemetech/alugames/principal/TesteBanco.kt
@@ -1,12 +1,15 @@
 package br.com.luizlemetech.alugames.principal
 
-import br.com.luizlemetech.alugames.dados.Banco
+import br.com.luizlemetech.alugames.dados.JogosDAO
 import br.com.luizlemetech.alugames.modelo.Jogo
 
 fun main() {
 
+    val jogo = Jogo("The Last of Us Part I", "https://cdn.cloudflare.steamstatic.com/steam/apps/1888930/header.jpg?t=1686864554", 5.99,"Uma aventura pós-apocalíptica de sobrevivência em um mundo infestado por zumbis e facções em conflito.")
+    val jogoDAO = JogosDAO()
 
+    //jogoDAO.adicionarJogo(jogo)
 
-    val listaJogos : List<Jogo> = Banco.getJogos()
+    val listaJogos : List<Jogo> = jogoDAO.getJogos()
     println(listaJogos)
 }
