e030f5a697dc1ed01bc42fd15deebb7eefd94bd4
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-01-10 22:28:40 -0300

    chore(feature/10.persistindo-dados-com-hibernate): migration de criacao da tabela

---
+++ AluGames/20250108-criacao-tabela.sql
@@ -0,0 +1,9 @@
+CREATE TABLE jogos(
+                      id INT AUTO_INCREMENT PRIMARY KEY,
+                      capa VARCHAR(255),
+                      descricao VARCHAR(255),
+                      preco DOUBLE,
+                      titulo VARCHAR(100)
+);
+
+COMMIT;
