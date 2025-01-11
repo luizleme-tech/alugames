6f566a0b38c8f567d6c4ce58193ded6491e8f5e3
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-01-11 12:04:56 -0300

    feat: implementa a classe de teste

--- AluGames/src/main/kotlin/br/com/luizlemetech/alugames/principal/TesteBanco.kt
+++ AluGames/src/main/kotlin/br/com/luizlemetech/alugames/principal/TesteBanco.kt
@@ -1,15 +1,20 @@
 package br.com.luizlemetech.alugames.principal
 
-import br.com.luizlemetech.alugames.dados.JogosDAO
+import br.com.luizlemetech.alugames.dados.config.Banco
+import br.com.luizlemetech.alugames.dados.dao.JogosDAO
 import br.com.luizlemetech.alugames.modelo.Jogo
 
 fun main() {
 
     val jogo = Jogo("The Last of Us Part I", "https://cdn.cloudflare.steamstatic.com/steam/apps/1888930/header.jpg?t=1686864554", 5.99,"Uma aventura pós-apocalíptica de sobrevivência em um mundo infestado por zumbis e facções em conflito.")
-    val jogoDAO = JogosDAO()
+    val jogo2 = Jogo("Dandara", "https://cdn.cloudflare.steamstatic.com/steam/apps/612390/header.jpg?t=1674055293", 9.99,"Um jogo de plataforma e ação com elementos de metroidvania, onde você controla a heroína Dandara em sua luta para libertar um mundo repleto de opressão e tirania.")
 
-    //jogoDAO.adicionarJogo(jogo)
+    val entityManager = Banco.getEntityManager()
 
+    val jogoDAO = JogosDAO(entityManager)
+    jogoDAO.adicionarJogo(jogo2)
     val listaJogos : List<Jogo> = jogoDAO.getJogos()
     println(listaJogos)
+
+    entityManager.close()
 }
