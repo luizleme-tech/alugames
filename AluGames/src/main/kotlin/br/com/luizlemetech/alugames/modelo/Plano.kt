3e987b02c7c5cb416a25b4acd85809a0d74caf33
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-01-13 20:14:09 -0300

    feat: repassado o id

--- AluGames/src/main/kotlin/br/com/luizlemetech/alugames/modelo/Plano.kt
+++ AluGames/src/main/kotlin/br/com/luizlemetech/alugames/modelo/Plano.kt
@@ -2,7 +2,7 @@ package br.com.luizlemetech.alugames.modelo
 
 import java.math.BigDecimal
 
-abstract class Plano(val tipo: String) {
+abstract class Plano(val tipo: String, var id: Int = 0) {
 
     open fun obterValor(aluguel: Aluguel): BigDecimal {
         return aluguel.jogo.preco * aluguel.periodo.emDias.toBigDecimal()
