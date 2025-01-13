2c328e304849cca3c5c635100b3de8837941f7c0
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-01-13 20:13:50 -0300

    feat: renomeado a variavel

--- AluGames/src/main/kotlin/br/com/luizlemetech/alugames/dados/dao/JogosDAO.kt
+++ AluGames/src/main/kotlin/br/com/luizlemetech/alugames/dados/dao/JogosDAO.kt
@@ -5,8 +5,8 @@ import br.com.luizlemetech.alugames.modelo.Jogo
 import javax.persistence.EntityManager
 
 class JogosDAO(manager: EntityManager) : DAO<Jogo, JogoEntity>(manager, JogoEntity::class.java) {
-    override fun toEntity(objeto: Jogo): JogoEntity {
-        return JogoEntity(objeto.titulo, objeto.capa, objeto.preco.toDouble(), objeto.descricao, objeto.id!!)
+    override fun toEntity(jogo: Jogo): JogoEntity {
+        return JogoEntity(jogo.titulo, jogo.capa, jogo.preco.toDouble(), jogo.descricao, jogo.id!!)
     }
 
     override fun toModel(entity: JogoEntity): Jogo {
