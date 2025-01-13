c180b490a685a9bed30929077a46a02a42c49f3c
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-01-13 20:10:22 -0300

    feat: implementado nova fun para formatar o BigDecimal

--- AluGames/src/main/kotlin/br/com/luizlemetech/alugames/utilitario/DoubleExtension.kt
+++ AluGames/src/main/kotlin/br/com/luizlemetech/alugames/utilitario/DoubleExtension.kt
@@ -1,5 +1,6 @@
 package br.com.luizlemetech.alugames.utilitario
 
+import java.math.BigDecimal
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.util.*
@@ -7,4 +8,8 @@ import java.util.*
 fun Double.formatarComDuasCasas() : Double {
     val formatador = DecimalFormat("#.00", DecimalFormatSymbols(Locale.US))
     return formatador.format(this).toDouble()
+}
+
+fun Double.paraBigDecimal(value: Double) : BigDecimal {
+    return BigDecimal(value).setScale(2, BigDecimal.ROUND_HALF_UP)
 }
