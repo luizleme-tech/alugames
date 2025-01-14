4d0ae5d57d57873148c5252312eaa78b615dd2c5
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-01-13 21:07:30 -0300

    feat: implementado os conversores para entity e model

--- AluGames/src/main/kotlin/br/com/luizlemetech/alugames/utilitario/JogoExtension.kt
+++ AluGames/src/main/kotlin/br/com/luizlemetech/alugames/utilitario/JogoExtension.kt
@@ -1,5 +1,6 @@
 package br.com.luizlemetech.alugames.utilitario
 
+import br.com.luizlemetech.alugames.dados.entities.JogoEntity
 import br.com.luizlemetech.alugames.modelo.InfoJogoJson
 import br.com.luizlemetech.alugames.modelo.Jogo
 
@@ -10,4 +11,12 @@ fun InfoJogoJson.criaJogo(): Jogo {
         this.preco,
         this.descricao
     )
+}
+
+fun Jogo.toEntity() : JogoEntity {
+    return JogoEntity(this.titulo, this.capa, this.preco.toDouble(), this.descricao, this.id!!)
+}
+
+fun JogoEntity.toModel() : Jogo {
+    return Jogo(this.titulo, this.capa, this.preco, this.descricao!!, this.id!!)
 }
